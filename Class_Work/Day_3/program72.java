d8d54d6642cf7caa42929d3612bb7e7579031059
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-07 21:48:50 +0530

    Add files via upload

---
+++ Class_Work/Day_3/program72.java
@@ -0,0 +1,19 @@
+// Display the factors of the given number
+
+class program72
+{
+    public static void main(String A[])
+    {
+        int iNo = 12;
+
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo/2; iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
