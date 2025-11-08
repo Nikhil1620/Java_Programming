ca0b5b8b89adac1a4b3fb4ed6e16e400e13c7262
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-08 10:14:33 +0530

    Add files via upload

---
+++ Class_Work/Day_2/program88.java
@@ -0,0 +1,49 @@
+// Display the factors of the given number
+
+import java.util.Scanner;
+class Number
+{
+    public long CalculateFactorial(int iNo)
+    {
+        int iCnt = 0;
+        long iMulti = 1;     //Important
+
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+
+        for(iCnt = iNo; iCnt >= 1; iCnt--)
+        {
+             iMulti = iMulti * iCnt; 
+        }
+
+        return iMulti;
+    }
+}// End of Number class
+
+class program88
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int iValue = sobj.nextInt();
+
+        long iResult = 0;
+
+        Number nobj = new Number();
+        iResult = nobj.CalculateFactorial(iValue);
+
+        System.out.println("The Factorial of "+iValue+" is "+iResult);
+
+
+        //Important
+        
+        sobj = null;
+        nobj = null;
+
+        System.gc();
+
+    }
+}
