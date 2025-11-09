fe020c6f1fbbd07ed81565f33a1c6ead294bf666
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-09 11:00:06 +0530

    Add files via upload

---
+++ Class_Work/Day_3/program103.java
@@ -0,0 +1,24 @@
+//Oop Design
+
+class ArrayX
+{
+    public int Arr[];
+    public int iSize;
+
+    public ArrayX(int No)
+    {
+        iSize = 0;
+        Arr = new int[iSize];
+    }
+}
+
+class program103
+{
+    public static void main(String A[])
+    {
+        ArrayX aobj1 = new ArrayX(5);
+        ArrayX aobj2 = new ArrayX(7);
+        ArrayX aobj3 = new ArrayX(3);
+
+    }
+}
