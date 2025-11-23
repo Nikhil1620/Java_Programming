d52099cdd8f80a0cc5fd7fa503ac3b48370bae7d
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-23 13:04:48 +0530

    Add files via upload

---
+++ Class_Work/Day_6/program180.java
@@ -0,0 +1,57 @@
+/*
+input:
+        iRow = 4
+        iCol = 4
+
+Output:
+        a b c d
+        a b c d
+        a b c d
+        a b c d
+
+*/
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+        char ch = 'a';
+
+        for(i = 1; i <= iRow; i++)
+        {
+            ch = 'a';       //Reset;
+
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print(ch+"\t");
+                ch++;          
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program180
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of coloumns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+
+}
