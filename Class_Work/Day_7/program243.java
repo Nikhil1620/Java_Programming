50a3c30fff3e0c66defdb361d8fbe8fd5ff93773
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-29 11:07:10 +0530

    Add files via upload

---
+++ Class_Work/Day_7/program243.java
@@ -0,0 +1,23 @@
+//Input : Hello
+//Output : H e l l o
+
+import java.util.Scanner;
+
+class program243
+{
+    public static void main(String A[]) throws Exception
+    {
+        int iCnt = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String : ");
+        String str = sobj.nextLine();
+
+        for(iCnt = 0; iCnt < str.length(); iCnt++)
+        {
+            System.out.println(str.charAt(iCnt));
+        }
+
+    }
+}
