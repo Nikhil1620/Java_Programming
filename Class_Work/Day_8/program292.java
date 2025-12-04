01f992fc0efd88b41ab44fa9473b8255652ed1a9
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-04 11:38:18 +0530

    Add files via upload

---
+++ Class_Work/Day_8/program292.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class program292
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {45,21,90,54,78};
+
+        //This is Foreach Loop
+        for(int no : Arr)
+        {
+            System.out.println(no);
+        }
+    }
+
+}
