9f8cf0f2d63218893e80390e8c508acd618dbfc7
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-19 23:08:41 +0530

    Add files via upload

---
+++ Class_Work/Day_9/program561.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class program561
+{
+    public static void main(String A[]) throws Exception
+    {
+        boolean bRet = false;
+        String FileName = null;
+        Scanner sobj = new Scanner(System.in);
+        File fobj = null;
+        
+        System.out.println("Enter the name of file : ");
+
+        FileName = sobj.nextLine();
+
+        FileWriter fwobj = new FileWriter(FileName);
+        
+        sobj.close();
+
+    }
+}
