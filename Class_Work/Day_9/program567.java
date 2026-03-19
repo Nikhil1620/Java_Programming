76d8f9989de699edd2a96bd67c6c5fcc185318a6
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-19 23:10:48 +0530

    Add files via upload

---
+++ Class_Work/Day_9/program567.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class program567
+{
+    public static void main(String A[]) throws Exception
+    {
+        boolean bRet = false;
+        String FileName = null;
+        Scanner sobj = new Scanner(System.in);
+        File fobj = null;
+        FileReader frobj = null;
+        
+        System.out.println("Enter the name of file : ");
+
+        FileName = sobj.nextLine();
+
+        fobj = new File(FileName);
+
+        if(fobj.exists())
+        {
+            frobj = new FileReader(FileName);
+
+            char Buffer[] = new char[50];
+
+            frobj.read(Buffer,0,13);
+
+            System.out.println("Data from file : "+new String(Buffer));
+        }
+        else
+        {
+            System.out.println("There is no such file.");
+        }
+        
+        if(frobj != null)
+        {
+            frobj.close();
+        }
+        
+        sobj.close();
+
+    }
+}
