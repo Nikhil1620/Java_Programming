6a2d67d3a7ab29133a7fd889e7ff5a8da38b583f
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-21 01:44:16 +0530

    Add files via upload

---
+++ Class_Work/Day__11/program615.java
@@ -0,0 +1,12 @@
+
+class program615
+{
+    public static void main(String A[])
+    {
+        int No = 10;
+
+        System.out.println("Value of No in Decimal is : "+No);
+        System.out.printf("Value of No in Decimal is : %d\n",No);
+        
+    }
+}
