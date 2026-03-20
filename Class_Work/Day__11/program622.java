e9c9b053ae6c8351a9429f302ce602d3f4a8aa46
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-21 01:46:14 +0530

    Add files via upload

---
+++ Class_Work/Day__11/program622.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+class program622
+{
+    public static void main(String A[])
+    {
+        
+        int iNo = 0;
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        iNo = sobj.nextInt(); 
+
+        int iDigit = 0;
+        int iCount1 = 0;
+        int iCount0 = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 2;
+            if(iDigit == 1)
+            {
+                iCount1 = iCount1 + 1;
+            }
+            else
+            {
+                iCount0 = iCount0 + 1;
+            }
+            System.out.print(iDigit);
+            iNo = iNo / 2;
+        }  
+        System.out.println();
+        System.out.println("The number of 1s in the Given number is "+iCount1);
+        System.out.println("The number of 0s in the Given number is "+iCount0);
+    }
+}
+
+/*
+  Decimal      Hexa_Decimal        Binary
+    0              0                0000
+    1              1                0001
+    2              2                0010
+    3              3                0011
+    4              4                0100
+    5              5                0101
+    6              6                0110
+    7              7                0111
+    8              8                1000
+    9              9                1001
+    10             a                1010
+    11             b                1011
+    12             c                1100
+    13             d                1101
+    14             e                1110
+    15             f                1111
+*/
