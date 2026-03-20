458a4fc2f9916f82fd95703ec293fa9f429292be
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-21 01:44:34 +0530

    Add files via upload

---
+++ Class_Work/Day__11/program616.java
@@ -0,0 +1,12 @@
+
+class program616
+{
+    public static void main(String A[])
+    {
+        int No = 10;
+
+        System.out.printf("Decimal : %d\n",No);
+        System.out.printf("Hexa_Decimal : %x\n",No);
+        
+    }
+}
