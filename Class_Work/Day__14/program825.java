19c0fcbc3deab95432d6ed4e8bd89cf541c72251
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-31 13:21:33 +0530

    Add files via upload

---
+++ Class_Work/Day__14/program825.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class program825
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = 0;
+        int iCount = 0;
+        int iDigit = 0;
+        int iSum = 0;
+        int iTemp = 0;
+        int iPow = 1;
+
+        System.out.println("Enter the number : ");
+        iNo = sobj.nextInt();
+
+        iTemp = iNo;
+
+        //Count numner of digits
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iCount++;
+            iNo = iNo / 10;
+        }
+
+        iNo = iTemp;
+        
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            iPow = (int)Math.pow(iDigit,iCount);
+
+            iSum = iSum + iPow;
+            iNo = iNo / 10;
+        }
+
+        if(iSum == iTemp)
+        {
+            System.out.println("Armstrong Number");
+        }
+        else
+        {
+            System.out.println("Not an Armstrong Number");
+        }
+    }
+}
