7f60b6371c20e6ecef1146b13113e881f7653940
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-31 13:21:48 +0530

    Add files via upload

---
+++ Class_Work/Day__14/program826.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class program824
+{
+    public static void main(String A[])
+    {
+        int iNo = 5678;
+
+        String str = Integer.toString(iNo);
+
+        System.out.println(str);
+        System.out.println(str.length());
+        
+    }
+}
