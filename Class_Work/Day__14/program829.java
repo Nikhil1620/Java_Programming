723da40ff18b57f2a5a4241244e5ecf9d38d119a
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-03-31 13:22:39 +0530

    Add files via upload

---
+++ Class_Work/Day__14/program829.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class program829
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int iNo = sobj.nextInt();
+
+        int iDigit = 0;
+        int iRev = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iRev = iRev * 10 + iDigit;
+            iNo = iNo / 10;
+        }
+        
+        System.out.println(iRev);
+    }
+}
