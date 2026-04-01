4ef27a9b5c6d73298e0b2649ae05e950d5f19f68
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-01 14:11:34 +0530

    Add files via upload

---
+++ Class_Work/Day__15/program841.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class program841
+{
+    public static void main(String A[])
+    {
+        /*Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the string : ");*/
+
+        String str = "    hello world    ";
+        System.out.println(str);
+        str = str.trim();
+        System.out.println(str);
+        System.out.println(str.length()); //Will count Space
+
+    }
+}
