67eadd0d459c362cf5ea81a3c42fcb76203be777
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-01 14:09:32 +0530

    Add files via upload

---
+++ Class_Work/Day__15/program834.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class program832
+{
+    public static void main(String A[])
+    {
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.sqrt(12));
+    }
+}
