1096a479306b11eb286899cd333cda227c3f0aeb
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-01 14:13:38 +0530

    Add files via upload

---
+++ Class_Work/Day__15/program848.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class program848
+{
+    public static void main(String A[])
+    {
+        /*Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the string : ");*/
+
+        String str = "  india   is   my  country ";
+         System.out.println(str);
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+"," ");
+
+        System.out.println(str.length()); //Will count Space
+
+        System.out.println(str);
+
+    }
+}
