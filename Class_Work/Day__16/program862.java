85dfe7c67ce88996162771d6fee9d1e33a3350c5
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-02 14:18:49 +0530

    Add files via upload

---
+++ Class_Work/Day__16/program862.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class program862
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+
+        //Step 0
+        String str = sobj.nextLine();
+        //Step 1
+        str = str.trim();
+
+        //step 2
+        str = str.replaceAll("//s+"," ");
+
+        //step 3
+        String Arr[] = str.split(" ");
+
+        System.out.println("Number of words are "+Arr.length);
+
+        int iCount = 0;
+
+        for(int i = 0; i < Arr.length; i++)    //"For each loop"
+        {
+            if(Arr[i].equals("india") == true)
+            {
+                iCount++;
+            }
+        }
+
+        System.out.println("Th frequency of india word is "+iCount);
+    }
+}
