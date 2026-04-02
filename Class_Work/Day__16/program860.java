c9904f0b485075c7be46b06c79e74f5f824be546
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2026-04-02 14:18:02 +0530

    Add files via upload

---
+++ Class_Work/Day__16/program860.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class program860
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+
+        //Step 0
+        String str = sobj.nextLine();
+        //Step 1
+        str = str.trim();
+
+        //step 2
+        str = str.replaceAll("//s+"," ");
+
+        //step 3
+        String Arr[] = str.split(" ");
+
+        System.out.println("Number of words are "+Arr.length);
+
+        int lLength = 0;
+
+        for(int i = 0; i < Arr.length; i++)    //"For each loop"
+        {
+            if(Arr[i].length() > lLength)
+            {
+                lLength = Arr[i].length();
+            }
+            
+        }
+
+        System.out.println("The length of the longest word in the string is "+lLength);
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i].length() == lLength)
+            {
+                System.out.println(Arr[i]+" is the longest word"+" has the length "+lLength);
+            }
+        }
+    }
+}
